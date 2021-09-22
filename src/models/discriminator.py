98803d20c8461b54d11a997c510071048031568e
ddcas <ddcastilloiglesias@gmail.com>
2021-09-22 19:00:18 +0200

    implemented discriminator model

---
+++ src/models/discriminator.py
@@ -0,0 +1,50 @@
+"""
+As described in https://arxiv.org/abs/2001.07466,
+"""
+
+import torch.nn as nn
+
+
+class Discriminator(nn.Module):
+    def __init__(self, patch_size, n=128):
+        super().__init__()
+
+        self.patch_size = patch_size
+        self.n = n
+
+        patch_factors = self._get_patch_factors()
+        channels = self._get_channels()
+
+        modules = []
+        for i, pf in enumerate(patch_factors):
+            modules.append(nn.Conv2d(*channels[i], pf, pf, padding=1))
+            if i > 0:
+                modules.append(nn.InstanceNorm2d(channels[i][-1]))
+            if i < len(patch_factors):
+                modules.append(nn.LeakyReLU(0.2, inplace=True))
+
+        modules.append(nn.Conv2d(channels[-1][-1], 1, 1, 1))
+
+        self.model = nn.Sequential(*modules)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.model(x)
+        x = self.sigmoid(x)
+
+        return x.mean((-2, -1)).squeeze()
+
+    def _get_patch_factors(self):
+        patch_factors = list(reversed([f for f in range(1, self.patch_size + 1)
+                                       if self.patch_size % f == 0][1:-1]))
+        if len(patch_factors) == 1:
+            patch_factors *= 2
+
+        return patch_factors
+
+    def _get_channels(self):
+        channels = [self.n*(2**i, self.n*(2**(i+1)))
+                    for i, _ in enumerate(self.patch_factors)]
+        channels[0] = (3, self.n*2)
+
+        return channels
